53d6f2d4f760d364fa5e482fb64f53450ae28dd7
Brandon Hernandez <82180734+Shoider@users.noreply.github.com>
2023-11-15 23:59:47 -0600

    Add files via upload

--- Compilar.bat
+++ Compilar.bat
@@ -1,7 +1,8 @@
+
 #!/bin/bash
     clear
 echo "<Inicion de analizador Sintactico>"
     flex -l Proyecto.l
     bison -dv Proyecto.y
-    gcc -o main  Proyecto.tab.c lex.yy.c -lfl
+    gcc -o main  Proyecto.tab.c lex.yy.c
 echo "<Fin>"
